d2e7da0d3913b5cf950b496e89cbd958db6b154c
Ben Kent <benkent@live.co.uk>
2025-04-01 14:21:08 +0200

    coverage report to ignore _tab.m files

--- tools/knots_functions/knots_leja.m
+++ tools/knots_functions/knots_leja.m
@@ -48,12 +48,6 @@ if nargin==4
     whichrho='prob';
 end
 
-% as a first step, we load the precomputed knots
-current_dir = fileparts(mfilename('fullpath'));
-% Build the relative path (e.g., to a file "data/input.txt" located in the "data" subfolder)
-relative_path = fullfile(current_dir, 'data');
-addpath(relative_path);
-
 switch type
             
     %--------------------------------------------------------
